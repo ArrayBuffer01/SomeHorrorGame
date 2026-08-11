8c1d87969dbf3c8b6183fdbbe1e0ef1fae3af5fc
ArrayBuffer01 <gamesawayhd@gmail.com>
2026-08-11 19:35:46 +0200

    Color switcher logic

--- Source/SomeHorrorGame/Private/Puzzle/ColorSwitcher.cpp
+++ Source/SomeHorrorGame/Private/Puzzle/ColorSwitcher.cpp
@@ -1,5 +1,43 @@
 // SomeHorrorGame TM
 
-
 #include "Puzzle/ColorSwitcher.h"
+#include "Components/SpotLightComponent.h"
 
+
+AColorSwitcher::AColorSwitcher()
+{
+	
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	SpotlightComponent = CreateDefaultSubobject<USpotLightComponent>(TEXT("SpotlightComponent"));
+
+	SetRootComponent(StaticMesh);
+	SpotlightComponent->SetupAttachment(StaticMesh);
+
+	CurrentColorIndex = 0;
+}
+
+void AColorSwitcher::Interact_Implementation(AActor* Interactor, UPrimitiveComponent* Component)
+{
+	CurrentColorIndex = (CurrentColorIndex + 1) % Colors.Num();
+
+	// UpdateLightState();
+
+	if (CurrentColorIndex == CorrectColorIndex)
+	{
+		Solve();
+	}
+	else if (bSolved)
+	{
+		UnSolve();
+	}
+}
+
+bool AColorSwitcher::CanInteract_Implementation(AActor* Interactor, UPrimitiveComponent* Component) const
+{
+	return true;
+}
+
+void AColorSwitcher::UpdateLightState()
+{
+	SpotlightComponent->LightColor = Colors[CurrentColorIndex];
+}
--- Source/SomeHorrorGame/Public/Puzzle/ColorSwitcher.h
+++ Source/SomeHorrorGame/Public/Puzzle/ColorSwitcher.h
@@ -4,14 +4,39 @@
 
 #include "CoreMinimal.h"
 #include "Puzzle/PuzzleActorBase.h"
+#include "Interaction/Interactable.h"
 #include "ColorSwitcher.generated.h"
+
+class USpotLightComponent;
 
 /**
  * 
  */
 UCLASS()
-class SOMEHORRORGAME_API AColorSwitcher : public APuzzleActorBase
+class SOMEHORRORGAME_API AColorSwitcher : public APuzzleActorBase, public IInteractable
 {
 	GENERATED_BODY()
-	
+public:
+	AColorSwitcher();
+
+	void Interact_Implementation(AActor* Interactor, UPrimitiveComponent* Component) override;
+	bool CanInteract_Implementation(AActor* Interactor, UPrimitiveComponent* Component) const override;
+
+	void UpdateLightState();
+public:
+	// Components
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<USpotLightComponent> SpotlightComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FColor> Colors;
+
+	UPROPERTY()
+	int CurrentColorIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int CorrectColorIndex;
 };
