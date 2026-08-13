19d354a5fe1bd75bdef62c180dbfd1711e7b231c
ArrayBuffer01 <gamesawayhd@gmail.com>
2026-08-13 12:13:16 +0200

    Add main menu controller

---
+++ Source/SomeHorrorGame/Private/Player/SHGMainMenuController.cpp
@@ -0,0 +1,5 @@
+// SomeHorrorGame TM
+
+
+#include "Player/SHGMainMenuController.h"
+
---
+++ Source/SomeHorrorGame/Public/Player/SHGMainMenuController.h
@@ -0,0 +1,17 @@
+// SomeHorrorGame TM
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/SHGBasePlayerController.h"
+#include "SHGMainMenuController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOMEHORRORGAME_API ASHGMainMenuController : public ASHGBasePlayerController
+{
+	GENERATED_BODY()
+	
+};
