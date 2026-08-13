f1a3ae3b628bd48cd357c8cc675216844c539d00
ArrayBuffer01 <gamesawayhd@gmail.com>
2026-08-13 11:24:03 +0200

    Add base controller

---
+++ Source/SomeHorrorGame/Private/Player/SHGBasePlayerController.cpp
@@ -0,0 +1,5 @@
+// SomeHorrorGame TM
+
+
+#include "Player/SHGBasePlayerController.h"
+
---
+++ Source/SomeHorrorGame/Public/Player/SHGBasePlayerController.h
@@ -0,0 +1,17 @@
+// SomeHorrorGame TM
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SHGBasePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOMEHORRORGAME_API ASHGBasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
